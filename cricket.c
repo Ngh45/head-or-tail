a02d13a054d7e8850f2fd121474c90dd54d54974
Ngh45 <75538865+Ngh45@users.noreply.github.com>
2022-09-18 20:08:48 +0600

    Add files via upload

--- cricket.c
+++ cricket.c
@@ -11,93 +11,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
 int luck(int a, int b);
-void bowling();
-void bating();
+int bowling(int over);
+int bating(int over);
 int main()
 {
-    int num, guess;
-    int choose;
-    srand(time(NULL));
-    num = rand() % 2;
-    printf("0 input is tail and 1 input is head\n");
-    scanf("%i", &guess);
-    choose = luck(num, guess); // you will bat or bowl
-    if (choose == 1 || choose == 0)
-    {
-        printf("You are bating\n");
-        bating();
-    }
-    else
-    {
-        printf("You are bowling\n");
-        bowling();
-    }
-    return 0;
+        int num, guess;
+        int choose;
+        int over, ball;
+        int bat, compBat;
+        srand(time(NULL));
+        num = rand() % 2;
+        printf(COLOR_GREEN "0 input is tail and 1 input is head\n");
+        scanf("%i", &guess);
+        choose = luck(num, guess); // you will bat or bowl
+        printf(COLOR_GREEN "How much over?\n");
+        scanf("%i", &ball);
+        over = ball * 6;
+        if (choose == 1 || choose == 0)
+        {
+                printf("You are bating\n");
+                bat = bating(over);
+                printf("\nNow your are bowling\n");
+                compBat = bowling(over);
+                if (bat > compBat)
+                {
+                        printf("\nYou win by %i runs", bat);
+                }
+                else
+                {
+                        printf("\nYou lose by %i runs", bat);
+                }
+        }
+        else
+        {
+                printf("You are bowling\n");
+                compBat = bowling(over);
+                printf("\nNow your are bating\n");
+                bat = bating(over);
+                if (bat > compBat)
+                {
+                        printf("\nYou win by %i runs", bat);
+                }
+                else
+                {
+                        printf("\nYou lose by %i runs", bat);
+                }
+        }
+        return 0;
 }
 int luck(int a, int b)
 {
-    if (a == 1 && a == b)
-    {
-        printf("Congratulation it's head\n");
-        return 1;
-    }
-    else if (a == 0 && a == b)
-    {
-        printf("Congratulation it's tail\n");
-        return 0;
-    }
-    else if (a == 0 && a != b)
-    {
-        printf("Sorry its tail\n");
-        return 2;
-    }
-    else
-    {
-        printf("Sorry its head\n");
-        return 2;
-    }
-}
-void bating()
-{
-    int bowling, bat[6];
-    int score = 0;
-    srand(time(NULL));
-    bowling = rand() % 6 + 1;
-    for (int i = 0; i < 6; i++)
-    {
-        scanf("%i", &bat[i]);
-        if (bat[i] != bowling)
+        if (a == 1 && a == b)
         {
-            score = score + bat[i];
-            continue;
+                printf("Congratulation it's head\n");
+                return 1;
+        }
+        else if (a == 0 && a == b)
+        {
+                printf("Congratulation it's tail\n");
+                return 0;
+        }
+        else if (a == 0 && a != b)
+        {
+                printf(COLOR_RED "Sorry its tail\n");
+                return 2;
         }
         else
         {
-            printf("!!!!!out!!!!!\n");
-            break;
+                printf(COLOR_RED "Sorry its head\n");
+                return 2;
         }
-    }
-    printf("your score=%i", score);
 }
-void bowling()
+int bating(int over)
 {
-    int bowling[6], bat;
-    int score = 0;
-    srand(time(NULL));
-    bat = rand() % 6 + 1;
-    for (int i = 0; i < 6; i++)
-    {
-        scanf("%i", &bowling[i]);
-        if (bowling[i] != bat)
+        int bowling, bat[18];
+        int run = 0;
+        for (int i = 0; i < over; i++)
         {
-            score = score + bat;
-            continue;
+                srand(time(NULL));
+                bowling = rand() % 6 + 1;
+                scanf("%i", &bat[i]);
+                if (bat[i] != bowling)
+                {
+                        printf("\t\t\t\t\t%i\n", bowling);
+                        run = run + bat[i];
+                        continue;
+                }
+                else
+                {
+                        printf(COLOR_RED "!!!!!out!!!!!\n");
+                        break;
+                }
         }
-        else
+        printf("your run=%i", run);
+        return run;
+}
+int bowling(int over)
+{
+        int bowling[18], bat;
+        int run = 0;
+        for (int i = 0; i < over; i++)
         {
-            break;
+                srand(time(NULL));
+                bat = rand() % 6 + 1;
+                scanf("%i", &bowling[i]);
+                if (bowling[i] != bat)
+                {
+                        printf("\t\t\t\t\t%i\n", bat);
+                        run = run + bat;
+                        continue;
+                }
+                else
+                {
+                        printf(COLOR_RED "!!!!!Computer is out!!!!!\n");
+                        break;
+                }
         }
-    }
-    printf("computer score=%i", score);
+        printf("computer run=%i", run);
+        return run;
 }
